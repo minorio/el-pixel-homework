dbee17268b0ee1086194af4eae934dd260c0ce27
minorio <mbajduk@mail.ru>
2025-12-03 17:29:37 +0300

    Delete folder

--- el-pixel-homework/src/app/app.module.server.ts
+++ el-pixel-homework/src/app/app.module.server.ts
@@ -3,7 +3,7 @@ import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { App } from './app';
 import { AppModule } from './app-module';
 import { serverRoutes } from './app.routes.server';
-
+ 
 @NgModule({
   imports: [AppModule],
   providers: [provideServerRendering(withRoutes(serverRoutes))],
