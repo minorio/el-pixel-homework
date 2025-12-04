14b34074b6a49e02d04a773157a4fcf697e025dc
minorio <mbajduk@mail.ru>
2025-12-04 15:42:59 +0300

    Added messages component and service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import {Component} from '@angular/core';
-import {HeroesComponent} from './heroes/heroes.component';
+import { Component } from '@angular/core';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeroesComponent],
+  imports: [HeroesComponent, MessagesComponent],
 })
 export class AppComponent {
   title = 'Tour of Heroes';
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,33 +1,35 @@
-import {Component} from '@angular/core';
-import {
-  NgIf,
-  NgFor,
-  UpperCasePipe,
-} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-
+import {Component, OnInit } from '@angular/core';
+import {NgFor} from '@angular/common';
+import {HeroDetailComponent} from '../hero-detail/hero-detail.component';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+// import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  imports: [
-    FormsModule,
-    NgIf,
-    NgFor,
-    UpperCasePipe,
-  ],
+  imports: [HeroDetailComponent, NgFor],
 })
-
 export class HeroesComponent {
-  heroes = HEROES;
-  selectedHero?: Hero;
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  ngOnInit(): void {
+  this.getHeroes();
+}
+  // heroes = HEROES;
+  heroes: Hero[] = [];
+
+  selectedHero!: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
-}
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
+}
